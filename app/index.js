5e86aa32d2c830cca68ef669572d05c2aee3005a
Evan Coakley <evancoakley1@gmail.com>
2017-08-15 13:31:50 -0500

    complete

--- app/index.js
+++
@@ -1,5 +0,0 @@
-const moment = require('moment');
-const chalk = require('chalk');
-moment().format();
-
-let now = moment()
--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-require('./app/index.js')
+const moment = require('moment');
+const chalk = require('chalk');
+moment().format();
+
+let now = moment()
+
+function seconds(now) {
+    let seconds = 0
+    seconds += +now.format("H") * 60 * 60
+    seconds += +now.format("m") * 60
+    seconds += +now.format("s")
+    return seconds
+}
+
+function dayLight(now) {
+    if (now.isDST()) {
+        return chalk.green("is")
+    } else {
+        return chalk.red("is not")
+    }
+}
+
+function leapYear(now) {
+    if (now.isLeapYear()) {
+        return chalk.green("is")
+    } else {
+        return chalk.red("is not")
+    }
+}
+console.log("It is " + chalk.blue(now.format("dddd, MMMM Do YYYY, h:mm:ss a")) + ".");
+console.log("It is the " + chalk.magenta(now.dayOfYear()) + "th day of the year.");
+console.log("It is " + chalk.cyan(seconds(now)) + " seconds into the day.");
+console.log("It " + dayLight(now) + " during daylight savings time");
+console.log("It " + leapYear(now) + " a leap year.");
